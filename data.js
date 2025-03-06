a871ce4019d0761234fbd65ab9900e056c6aece2
Devansh Tyagi <118974690+devanshtyagi26@users.noreply.github.com>
2025-03-06 09:51:16 +0530

    Creating hashmap file

---
+++ data.js
@@ -0,0 +1,83 @@
+const svgConfigsMap = new Map([
+  [
+    "Sec1",
+    new Map([
+      [
+        "Sector32",
+        {
+          row_count: 7,
+          col_count: 11,
+          spacingX: 50,
+          spacingY: 100,
+          radius: 15,
+          rows: [
+            new Map([
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+            ]),
+          ],
+        },
+      ],
+      [
+        "Sector32",
+        {
+          row_count: 7,
+          col_count: 11,
+          spacingX: 50,
+          spacingY: 100,
+          radius: 15,
+          rows: [
+            new Map([
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+              [
+                ["config", { rowName: "A", align: "right" }],
+                ["cols", new Map(["1", "2", "3", "4"])],
+              ],
+            ]),
+          ],
+        },
+      ],
+    ]),
+  ],
+]);
